5ae3785b5993604eea9fb45190fd5f5cdc8fd254
Chaitanya Yenamandra <chaitanya0368@gmail.com>
2024-01-31 20:21:33 +0530

    Jenkins Demo code
    Signed-off-by: Chaitanya Yenamandra <chaitanya0368@gmail.com>

---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1 {
+
+	public void callHello() {
+		
+		System.out.println("This is Jenkins Test ");
+	}
+	public static void main(String[] args) {
+
+		Test1 one = new Test1();
+		one.callHello();
+	}
+
+}
